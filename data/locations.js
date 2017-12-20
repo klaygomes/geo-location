1c6cf125781f0184fa3a7901b6dd5826783d094b
Cleiton Loiola <klaygomes@gmail.com>
2017-12-19 22:49:46 -0200

    chore: include mock data
    We need mock data to perform tests and to configure the examples showed
    on storybook

---
+++ data/locations.js
@@ -0,0 +1,4 @@
+export default {
+  myLocation: {lat: -23.605420, lng: -46.602207},
+  hostnameLocation: {lat: -15.791316, lng: -47.889569}
+}
---
+++ data/panelInformation.js
@@ -0,0 +1,9 @@
+export default {
+  query: '201.43.52.26',
+  contry: 'Brazil',
+  regionName: 'Sao Paulo',
+  city: 'São Paulo',
+  zipCode: '--',
+  lat: -23.5733,
+  lng: -46.6417
+}
