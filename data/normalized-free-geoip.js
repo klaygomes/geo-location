714782f655d68afc3f958b484066da9be63841d5
Cleiton Loiola <klaygomes@gmail.com>
2017-12-20 18:48:56 -0200

    chore: include new mock data
    Include and rename mock data that is used throughout the application

---
+++ data/normalized-free-geoip.js
@@ -0,0 +1,13 @@
+export default {
+  'city': 'Mountain View',
+  'contryName': 'United States',
+  'countryCode': 'US',
+  'ip': '172.217.10.4',
+  'lat': 37.4192,
+  'lng': -122.0574,
+  'metroCode': 807,
+  'regionCode': 'CA',
+  'regionName': 'California',
+  'timeZone': 'America/Los_Angeles',
+  'zipCode': '94043'
+}
---
+++ data/raw-free-geoip.js
@@ -0,0 +1,13 @@
+export default {
+  'ip': '172.217.10.4',
+  'country_code': 'US',
+  'country_name': 'United States',
+  'region_code': 'CA',
+  'region_name': 'California',
+  'city': 'Mountain View',
+  'zip_code': '94043',
+  'time_zone': 'America/Los_Angeles',
+  'latitude': 37.4192,
+  'longitude': -122.0574,
+  'metro_code': 807
+}
