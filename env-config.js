bdbe8b8012231e299db32c4fa503b035e703478b
Cleiton Loiola <klaygomes@gmail.com>
2017-12-19 22:47:51 -0200

    style: format file according to code conversion rules

--- env-config.js
+++ env-config.js
@@ -1,9 +1,9 @@
 const defaultConfiguration = {
-    API_URL: 'https://freegeoip.net/json/'
-  }
-  
-  module.exports = Object.assign({},
-    defaultConfiguration, process.env.NODE_ENV === 'production' && {
-      /* production configuration goes here */
-  
-    })
+  API_URL: 'https://freegeoip.net/json/'
+}
+
+module.exports = Object.assign({},
+  defaultConfiguration, process.env.NODE_ENV === 'production' && {
+    /* production configuration goes here */
+
+  })
