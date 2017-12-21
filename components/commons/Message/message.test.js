85550335bb086b71251430943b671d1dbde4b4d2
Cleiton Loiola <klaygomes@gmail.com>
2017-12-21 11:11:46 -0200

    test: include missing tests for darger property

--- components/commons/Message/message.test.js
+++ components/commons/Message/message.test.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import 'jest-styled-components'
+import toJson from 'enzyme-to-json'
 
 import Message from './'
+import { themes } from '../../../theme'
+
+const theme = themes.dark
 
 describe('<Message />', () => {
   it('should render without crashing', () => {
@@ -11,5 +15,13 @@ describe('<Message />', () => {
   })
   it('should has a displayName', () => {
     expect(Message.displayName).toBeTruthy()
+  })
+  it('be default', () => {
+    const wrapper = shallow(<Message theme={theme} />)
+    expect(toJson(wrapper)).toHaveStyleRule('background-color', theme.success)
+  })
+  it('be error', () => {
+    const wrapper = shallow(<Message danger theme={theme} />)
+    expect(toJson(wrapper)).toHaveStyleRule('background-color', theme.error)
   })
 })
