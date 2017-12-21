2959e4a2ce65dd85a1034e13f94921df90574a15
Cleiton Loiola <klaygomes@gmail.com>
2017-12-20 23:40:48 -0200

    feat: improve page layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
+/* global THEME_NAME */
+
 import React from 'react'
-import styled from 'styled-components'
-import media from '../utils/media'
+import styled, { injectGlobal } from 'styled-components'
+
+import { typography } from '../theme'
+injectGlobal`
+  body {
+    font-family: ${typography.fontFamily};
+    padding: 0 auto;
+    margin: 0;
+    font-size: 26px;
+  }
+`
 
 const Holder = styled.div`
   max-width: 75%;
@@ -11,16 +22,27 @@ const Holder = styled.div`
 
 export default () => (<Holder>
   <h1>Welcome to my solution page</h1>
-  <p>These are my solutions to this frontend challenge</p>
+  <p>GeoLocation is a working web application which aims to retrieve details about the user's location and allow the user to find out the physical location of websites, by displaying its hosting position on a map.</p>
+
+  <p>For this solution the following technologies were chosen:</p>
+  <ul>
+    <li>React</li>
+    <li>Redux</li>
+    <li>Redux Sagas</li>
+    <li>Next.js</li>
+    <li>Jest</li>
+    <li>Enzyme</li>
+    <li>Storybook</li>
+  </ul>
   <h2>What was done</h2>
-  <p>All versions are SSR enabled, responsive and acessible (you can use only your keyboard for example and a screen reader)</p>
-  <p>All versions were tested on Chrome, Safari, Firefox and Edge (olders browser were neglected althouh there is polyfill)</p>
+  <p>GeoLocation was written with SSR, responsiveness, acessibleness in mind</p>
+  <p>It was tested on Chrome, Safari, Firefox and Edge (olders browser were neglected althouh there is polyfill)</p>
   <h2>Options</h2>
-  <h3>Simple solution with React internal state management</h3>
-  <p> <a href='/with-local-state' target='_blank'>Click here</a> to access</p>
   <h3>Solution using React and Redux</h3>
   <p>This solution uses redux-sagas as effects management (SSR rendered), you can use Redux Dev Tools to check for Events and etc.
     <a href='/with-redux' target='_blank'>Click here</a> to access</p>
+  <h3>Simple solution with React internal state management</h3>
+  <p> <a href='/with-local-state' target='_blank'>Click here</a> to access</p>
   <h3>Source Code</h3>
   <p>Source code can be accessed <a href='https://bitbucket.org/klaygomes/ui-challenge/src' target='_blank'>here</a>.</p>
   <h3>Storybook</h3>
@@ -36,7 +58,7 @@ export default () => (<Holder>
     <li>I18N</li>
   </ul>
   <p>You can acess this page using your cellphone</p>
-  <img src='/static/geocode-site.jpg' />
+  <img src='/static/geocode-site.png' />
   <div>My linkedin: https://www.linkedin.com/in/klaygomes/</div>
   <div>Phone: +55 11 977387576</div>
 </Holder>)
